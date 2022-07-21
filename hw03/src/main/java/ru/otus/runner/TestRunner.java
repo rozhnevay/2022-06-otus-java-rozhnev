d1b95842c724b51888119bd9f5a18051d6bb12dc
Aleksey Rozhnev <rozhnevay@gmail.com>
2022-07-21 18:16:33 +0500

    hw03 - fixes after review

--- hw03/src/main/java/ru/otus/runner/TestRunner.java
+++ hw03/src/main/java/ru/otus/runner/TestRunner.java
@@ -1,47 +1,50 @@
 package ru.otus.runner;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import ru.otus.annotation.After;
 import ru.otus.annotation.Before;
 import ru.otus.annotation.Test;
-import ru.otus.test.TestClass;
+import ru.otus.model.TestResult;
 
 public class TestRunner {
-    private static final String EXCEPTION_ERR_MSG = "%s %s: %s";
-
     private final String testClassName;
 
     public TestRunner(String testClassName) {
         this.testClassName = testClassName;
     }
 
-    public void run() throws ClassNotFoundException {
+    public TestResult run() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<?> clazz = Class.forName(testClassName);
         Method[] methods = clazz.getDeclaredMethods();
-        List<Method> beforeMethods = filterMethodsByAnnotation(methods, Before.class.getName());
-        List<Method> testMethods = filterMethodsByAnnotation(methods, Test.class.getName());
-        List<Method> afterMethods = filterMethodsByAnnotation(methods, After.class.getName());
+        List<Method> beforeMethods = filterMethodsByAnnotation(methods, Before.class);
+        List<Method> testMethods = filterMethodsByAnnotation(methods, Test.class);
+        List<Method> afterMethods = filterMethodsByAnnotation(methods, After.class);
 
         int cntError = 0;
         int cntTotal = 0;
+        List<TestResult.MethodException> exceptionList = new ArrayList<>();
 
         for (Method testMethod : testMethods) {
-            final TestClass testClass = new TestClass();
+            Constructor constructor = clazz.getConstructor();
+            Object testClassInstance = constructor.newInstance();
             beforeMethods.forEach(method -> {
                 try {
-                    method.invoke(testClass);
+                    method.invoke(testClassInstance);
                 } catch (Exception e) {
-                    System.out.println(String.format(EXCEPTION_ERR_MSG, Before.class.getName(), method.getName(), e.getCause()));
+                    exceptionList.add(new TestResult.MethodException(method, e));
                 }
             });
 
             try {
-                testMethod.invoke(testClass);
+                testMethod.invoke(testClassInstance);
             } catch (Exception e) {
-                System.out.println(String.format(EXCEPTION_ERR_MSG, Test.class.getName(), testMethod.getName(), e.getCause()));
+                exceptionList.add(new TestResult.MethodException(testMethod, e));
                 cntError++;
             } finally {
                 cntTotal++;
@@ -49,23 +52,22 @@ public class TestRunner {
 
             afterMethods.forEach(method -> {
                 try {
-                    method.invoke(testClass);
+                    method.invoke(testClassInstance);
                 } catch (Exception e) {
-                    System.out.println(String.format(EXCEPTION_ERR_MSG, After.class.getName(), method.getName(), e.getCause()));
+                    exceptionList.add(new TestResult.MethodException(method, e));
                 }
             });
         }
-        System.out.println("Total number of tests: " + cntTotal);
-        System.out.println("Number of failed tests: " + cntError);
 
+        return new TestResult(cntTotal, cntError, exceptionList);
     }
 
-    private List<Method> filterMethodsByAnnotation(Method[] methods, String annotationName) {
+    private List<Method> filterMethodsByAnnotation(Method[] methods, Class annotationClass) {
         return Arrays.stream(methods)
             .filter(
                 method ->
                     Arrays.stream(method.getDeclaredAnnotations())
-                        .anyMatch(annotation -> annotation.annotationType().getName().equals(annotationName))
+                        .anyMatch(annotation -> annotation.annotationType().equals(annotationClass))
             ).collect(Collectors.toList());
     }
 }
