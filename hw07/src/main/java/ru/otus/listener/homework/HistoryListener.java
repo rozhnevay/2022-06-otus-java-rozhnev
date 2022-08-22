9808bc6137c7c8877ae7c95c1744063db8ee5b7c
Aleksey Rozhnev <rozhnevay@gmail.com>
2022-08-22 14:12:03 +0500

    hw07

--- hw07/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -1,18 +1,23 @@
 package ru.otus.listener.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
 import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    private final Map<Long, Message> messageHistoryMap = new HashMap<>();
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        messageHistoryMap.put(msg.getId(), msg.copy());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.of(messageHistoryMap.get(id));
     }
 }
---
+++ hw07/src/main/java/ru/otus/processor/ProcessorSwapFields11And12.java
@@ -0,0 +1,11 @@
+package ru.otus.processor;
+
+import ru.otus.model.Message;
+
+public class ProcessorSwapFields11And12 implements Processor {
+
+    @Override
+    public Message process(Message message) {
+        return message.toBuilder().field11(message.getField12()).field12(message.getField11()).build();
+    }
+}
